146357bd3b587d637743910f7fa6c6595e6b1092
Lemonlust <Johnreygun@gmail.com>
2022-04-11 11:03:58 -0800

    Initial commit

---
+++ JinxMod/Content/Achievements/MasteryAchievement.cs
@@ -0,0 +1,17 @@
+﻿using RoR2;
+using System;
+using UnityEngine;
+
+namespace JinxMod.Modules.Achievements
+{
+    internal class MasteryAchievement : BaseMasteryUnlockable
+    {
+        public override string AchievementTokenPrefix => JinxPlugin.DEVELOPER_PREFIX + "_HENRY_BODY_MASTERY";
+        public override string AchievementSpriteName => "texMasteryAchievement";
+        public override string PrerequisiteUnlockableIdentifier => JinxPlugin.DEVELOPER_PREFIX + "_HENRY_BODY_UNLOCKABLE_REWARD_ID";
+
+        public override string RequiredCharacterBody => "JinxBody";
+                                                             //difficulty coeff 3 is monsoon. 3.5 is typhoon for grandmastery skins
+        public override float RequiredDifficultyCoefficient => 3;
+    }
+}
